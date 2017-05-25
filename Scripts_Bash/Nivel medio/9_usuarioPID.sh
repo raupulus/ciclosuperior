be996dd542b70c926595734ac3642fc1acc11141
fryntiz <tecnico@fryntiz.es>
2017-05-25 20:46:09 +0200

    Terminado ej9

--- Scripts_Bash/Nivel medio/9_usuarioPID.sh
+++ Scripts_Bash/Nivel medio/9_usuarioPID.sh
@@ -7,3 +7,28 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+miUID=`id | cut -d "=" -f2 | cut -d "(" -f1`
+
+if [ $# -ne 1 ]; then
+	echo "Error en el número de parámetros pasados"
+	exit 1
+fi
+
+echo "Introduce un número"
+while true
+do
+	read input
+	if [ $input -eq $miUID ]; then
+		echo "Has acertado"
+		break
+	elif [ $input -lt $miUID ]; then
+		echo "El UID es mayor"
+	elif [ $input -gt $miUID ]; then
+		echo "El UID es menor"
+	else
+		echo "Error no expecificado"
+		exit 1
+done
+
+exit 0
