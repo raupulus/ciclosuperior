5a09f592f17edbf522b97e3c25c4f21aa9402d21
fryntiz <tecnico@fryntiz.es>
2017-05-25 18:45:19 +0200

    Comprobando entrada

--- Scripts_Bash/Nivel medio/5_divisible.sh
+++ Scripts_Bash/Nivel medio/5_divisible.sh
@@ -3,8 +3,11 @@ TMP=$1
 MODULO=""
 
 if [ $# = 1 ]; then
-	if [ $1 < 100 ]
-	echo "Es menor de 101 y existe parametro de entrada"
+	if [ $1 -lt 101 ]; then
+		echo "Es menor de 101 y existe parametro de entrada"
+	else
+		echo "Es mayor o igual a 101"
+	fi
 else
 	echo "Se necesita un número como parámetro"
 	echo "Sintaxis: script.sh valor"
