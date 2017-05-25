3c658b29f7d6a049fc688c8c0815e30dea3cf149
fryntiz <tecnico@fryntiz.es>
2017-05-25 22:15:07 +0200

    Descripción del objetivo para el script

--- Scripts_Bash/Nivel medio/11_compara2.sh
+++ Scripts_Bash/Nivel medio/11_compara2.sh
@@ -7,3 +7,9 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+# Script que toma como parámetros una serie de nombres de fichero de texto
+# y muestra en la salida estándar el nombre del fichero que tenga más líneas.
+# Si hay más de un fichero con el número máximo de líneas debe mostrar el
+# nombre de todos ellos. La opción –l de esta orden sirve para que muestre
+# en la salida estándar el número de líneas del fichero que se pase como
+# parámetro, junto con su nombre.
