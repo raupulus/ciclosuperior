563778991d781f83b4525977665b94e68599a60c
fryntiz <tecnico@fryntiz.es>
2017-05-25 20:16:10 +0200

    Script acabado

--- Scripts_Bash/Nivel medio/7_infousu.sh
+++ Scripts_Bash/Nivel medio/7_infousu.sh
@@ -8,3 +8,26 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
+CONTADOR="0"
+VUELTAS="3"
+LOG="usuarios.log"
+
+if [ -f $LOG ]; then
+	touch $LOG
+else
+	echo "" >> $LOG
+	echo "" >> $LOG
+	echo "############## NUEVA SESIÓN ################" >> $LOG
+	echo "" >> $LOG
+fi
+
+while true
+do
+	if [ $CONTADOR -ge $VUELTAS ]; then
+		break
+	fi
+	echo "" >> $LOG
+	echo "`date` | `users | wc -w` | `users`" >> $LOG
+	let CONTADOR=$CONTADOR+1
+	sleep 5s
+done
