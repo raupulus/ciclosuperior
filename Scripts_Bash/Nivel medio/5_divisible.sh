2ed7ed2c546363a715445b9c813ccbf7d5e0b236
fryntiz <tecnico@fryntiz.es>
2017-05-25 18:48:52 +0200

    Comentados errores

--- Scripts_Bash/Nivel medio/5_divisible.sh
+++ Scripts_Bash/Nivel medio/5_divisible.sh
@@ -4,12 +4,13 @@ MODULO=""
 
 if [ $# = 1 ]; then
 	if [ $1 -lt 101 ]; then
-		echo "Es menor de 101 y existe parametro de entrada"
+		echo "Es menor de 101, por lo tanto no es divisible... espabila!"
+		exit 1
 	else
-		echo "Es mayor o igual a 101"
+		echo "El número introducido es $1"
 	fi
 else
-	echo "Se necesita un número como parámetro"
+	echo "Se necesita un número como parámetro (solo un número)"
 	echo "Sintaxis: script.sh valor"
 	exit 1
 fi
