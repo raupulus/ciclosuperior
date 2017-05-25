c90aee6ae436510714a48a04640cff00c2f54dd3
fryntiz <tecnico@fryntiz.es>
2017-05-25 23:51:32 +0200

    change

--- Scripts_Bash/Nivel medio/11_compara2.sh
+++ Scripts_Bash/Nivel medio/11_compara2.sh
@@ -7,6 +7,7 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
 # Script que toma como parámetros una serie de nombres de fichero de texto
 # y muestra en la salida estándar el nombre del fichero que tenga más líneas.
 # Si hay más de un fichero con el número máximo de líneas debe mostrar el
