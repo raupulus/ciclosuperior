ea4c3a4d2cf58383221836447f7e4cff91c8910f
fryntiz <tecnico@fryntiz.es>
2017-05-25 19:41:40 +0200

    ayuda.sh terminado

--- Scripts_Bash/Nivel medio/6_ayuda.sh
+++ Scripts_Bash/Nivel medio/6_ayuda.sh
@@ -12,10 +12,20 @@ function info() {
 	echo "Solo puede recibirse como parámetros:"
 	echo "-h muestra esta ayuda"
 	echo "El nombre de un archivo para comprobar sus permisos"
+	echo ""
+	echo "Sintaxis: sh $0 -h"
+	echo "Sintaxis: sh $0 archivo"
 }
 
 if [ $# -eq 1 ]; then
-
+	if [ $1 = "-h" ]; then
+		echo "Accediendo a la ayuda:"
+		info
+	elif [ -f $1 ]; then
+		echo "Permisos para este archivo: `ls -l $1 | cut -d " " -f1`"
+	else
+		echo "El archivo no existe o no se puede leer"
+	fi
 else
 	info
 	exit 1
