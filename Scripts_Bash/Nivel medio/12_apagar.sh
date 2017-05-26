9a6fe08774d559dffa5f32c64210f7deb884d5f8
fryntiz <tecnico@fryntiz.es>
2017-05-26 02:52:55 +0200

    Descripción del script

--- Scripts_Bash/Nivel medio/12_apagar.sh
+++ Scripts_Bash/Nivel medio/12_apagar.sh
@@ -7,3 +7,10 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+#Escribe un script llamado apagar.sh que pasando por argumentos permita:
+# • Reiniciar el equipo ahora (opción –n)
+# • Reiniciar el equipo dentro de N minutos (opción –l) (el valor de N se pasa
+#   como segundo parámetro)
+# Si el usuario no escribe ningún parámetro entonces nos mostrará las listas
+# de opciones.
