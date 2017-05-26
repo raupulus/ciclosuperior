0ce8ac1ccb68412a8e70b0c87413ba9d2ffa1cdb
fryntiz <tecnico@fryntiz.es>
2017-05-26 09:42:27 +0200

    Descripción del script

--- Scripts_Bash/Nivel medio/14_intervalo.sh
+++ Scripts_Bash/Nivel medio/14_intervalo.sh
@@ -7,3 +7,9 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+# Crea un script llamado intervalo.sh donde el usuario introduce dos números
+# enteros y deben devolverse todos los números enteros del intervalo formado
+# por los dos números. El programa debe comprobar que se pasan sólo 2
+# números y que el segundo es mayor que el primero, en caso contrario mostrar
+# mensaje de error.
