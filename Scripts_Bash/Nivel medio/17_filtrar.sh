997d6f93321ec78cb8653000bedbd906c0c215f8
fryntiz <tecnico@fryntiz.es>
2017-05-26 10:47:39 +0200

    Objetivo del script

--- Scripts_Bash/Nivel medio/17_filtrar.sh
+++ Scripts_Bash/Nivel medio/17_filtrar.sh
@@ -7,3 +7,12 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+# Realiza un script que admite un número indeterminado de parámetros. Si el
+# parámetro es un fichero y ocupa más de 50 bytes se borrará. Si es un
+# directorio se pedirá confirmación antes de borrarlo. En cualquier otro caso se
+# mostrará un mensaje de error “no se procesa el argumento: <argumento>”.
+# Sólo se procesará el directorio de trabajo.
+
+
+
