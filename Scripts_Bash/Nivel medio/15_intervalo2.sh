08631a6caf5c748d819c5c4f9ed991aa6264988c
fryntiz <tecnico@fryntiz.es>
2017-05-26 09:59:12 +0200

    Invertir orden si los valores no coinciden para el rango

--- Scripts_Bash/Nivel medio/15_intervalo2.sh
+++ Scripts_Bash/Nivel medio/15_intervalo2.sh
@@ -25,6 +25,10 @@ if [ $INICIO -lt $FIN ]; then
 	clear
 	echo "Mostrando rango"
 	seq $INICIO $FIN
+elif [ $INICIO -gt $FIN ]; then
+	clear
+	echo "Invirtiendo posición de los números y mostrando rango"
+	seq $FIN $INICIO
 else
 	echo "No se cumplen los requisitos"
 	echo "Se necesita un segundo número mayor que el primero"
