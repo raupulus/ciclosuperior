4bc8240e47bfade1f65397e8eb27a44de5e87438
fryntiz <tecnico@fryntiz.es>
2017-05-26 10:03:23 +0200

    función para menú

--- Scripts_Bash/Nivel medio/16_menu.sh
+++ Scripts_Bash/Nivel medio/16_menu.sh
@@ -7,3 +7,22 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+# Crea un script que muestre el siguiente menú al usuario con las siguientes opciones:
+# 1. Copiar un archivo a otro directorio
+# 2. Mover un archivo a otro directorio
+# 3. Cambiar de nombre a un archivo
+# 4. Borrar archivo
+# 6. Salir
+
+function menu() {
+	echo 1. Copiar un archivo a otro directorio
+	echo 2. Mover un archivo a otro directorio
+	echo 3. Cambiar de nombre a un archivo
+	echo 4. Borrar archivo
+	echo 6. Salir
+}
+
+while true; do
+	echo "entra"
+done
