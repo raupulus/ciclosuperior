481ed004197a3ddef49e3009f0411084ad6e61a9
fryntiz <tecnico@fryntiz.es>
2017-05-26 09:54:40 +0200

    Objetivo del script

--- Scripts_Bash/Nivel medio/15_intervalo2.sh
+++ Scripts_Bash/Nivel medio/15_intervalo2.sh
@@ -8,3 +8,6 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
+# Modifica el programa anterior haciendo que si el segundo parámetro es más
+# pequeño que el primero, los intercambie dentro del script. Tendremos que
+# comprobar que los números son distintos.
