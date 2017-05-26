362f2ec545031d7a0f2a2483cb38c3af1167ce5d
fryntiz <tecnico@fryntiz.es>
2017-05-26 09:55:21 +0200

    comenzando a partir del script 14

--- Scripts_Bash/Nivel medio/15_intervalo2.sh
+++ Scripts_Bash/Nivel medio/15_intervalo2.sh
@@ -11,3 +11,21 @@
 # Modifica el programa anterior haciendo que si el segundo parámetro es más
 # pequeño que el primero, los intercambie dentro del script. Tendremos que
 # comprobar que los números son distintos.
+
+INICIO=0
+FIN=0
+
+echo "Introduce el primero número (Inicio del rango)"
+read INICIO
+
+echo "Introduce el segundo número (Fin del rango)"
+read FIN
+
+if [ $INICIO -lt $FIN ]; then
+	clear
+	echo "Mostrando rango"
+	seq $INICIO $FIN
+else
+	echo "No se cumplen los requisitos"
+	echo "Se necesita un segundo número mayor que el primero"
+fi
